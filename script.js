d6df5d86744b5112061166b4b6c66f91e29a7184
Pulkit Singh Baliyan <duke79@users.noreply.github.com>
2017-01-21 22:33:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,38 @@
 
 (function() {
     'use strict';
+            
+    var usersJson;
+    var usersStr = localStorage.getItem("users");
+    console.log("usersStr= "+usersStr);
+    if(usersStr===null || usersStr==="null")
+    {
+        usersJson = [{"id":"jagga","days":15}];
+        console.log("usersJson= "+usersJson);
+        console.log("stringified= "+JSON.stringify(usersJson));
+        localStorage.setItem("users",JSON.stringify(usersJson));
+    }
+    else
+    {
+        usersJson = JSON.parse(usersStr);
+    }
+    console.log("usersJson= "+usersJson);      
+    //var user = {};
+      //  user.days = "234";
+      //  user.id = "myId";
+    //usersJson.push(user);
+    //localStorage.setItem("users",JSON.stringify(usersJson));
+    //localStorage.setItem("users","null");
+        
+    $.each($(".tagline > span.flair"), function( i, l ){
+        var days = $(this).attr("title");
+        var id = $(this).prev().text();
+        var user = {};
+        user.days = days;
+        user.id = id;        
+        usersJson.push(user);
+        localStorage.setItem("users",JSON.stringify(usersJson));        
+    });
 
     console.log("redditscript");
     var limit=5;
@@ -17,8 +49,8 @@
     console.log(i);
     if(i<limit && i>-1)
     {
-        var pos=25*i;
-        window.location.href="https://www.reddit.com/r/NoFap/new/?count="+pos+"&after=t3_5pat86";        
+        var pos=25*i;        
+        window.location.href="https://www.reddit.com/r/NoFap/?count="+pos+"&after=t3_5pb1wu";        
     }
     else if(i>=limit)
     {
